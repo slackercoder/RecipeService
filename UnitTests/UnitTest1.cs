9326431bc2d9514bc26f6006831b62526e381619
SlackerCoder <mario.folino@gmail.com>
2012-06-01 20:22:03 -0400

    Added nUnit 2.5.9 + a unit test. Removed the other test projects from the library for now.

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(0 == 0);
+        }
+    }
+}
